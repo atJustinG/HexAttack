27908518dc4454cae8fec018337cd218707caf9a
atJustinG <45684351+atJustinG@users.noreply.github.com>
2024-08-27 21:28:53 +0200

    Camera movement complete
    completed camera movement
    To-Dos:
    Zoom organic
    Camera spawn tilegrid center

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -3,8 +3,7 @@ extends Node3D
 
 var player: Node3D
 var speed = 3
-var dirZ: Vector3 = Vector3.ZERO
-var dirX: Vector3 = Vector3.ZERO
+var dir: Vector3 = Vector3.ZERO
 var minZoom: int = 2
 var maxZoom: int = 9
 
@@ -21,8 +20,8 @@ func _physics_process(delta: float) -> void:
 	move(delta)
 
 func move(delta: float) -> void:
-	dirZ = Vector3.ZERO
-	dirX = Vector3.ZERO
+	dir = Vector3(0,0,0)
+	
 	if Input.is_action_pressed("move_up"):
 		move_up()
 	if Input.is_action_pressed("move_down"):
@@ -31,21 +30,22 @@ func move(delta: float) -> void:
 		move_left()
 	if Input.is_action_pressed("move_right"):
 		move_right()
-	var direction = dirZ + dirX
+	var direction = Vector3(dir.x, 0., dir.z)
+	print("direction", direction)
 	direction = direction.normalized()
 	player.transform.origin -= direction * speed * delta
 
 func move_up() -> void:
-	dirZ += player.transform.basis.z 
+	dir += player.transform.basis.z
 
 func move_down() ->void:
-	dirZ -= player.transform.basis.z
+	dir -= player.transform.basis.z
 
 func move_left() ->void:
-	dirX += player.transform.basis.x
+	dir += player.transform.basis.x
 
 func move_right() ->void:
-	dirX -= player.transform.basis.x
+	dir -= player.transform.basis.x
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("rotate_cam"):
